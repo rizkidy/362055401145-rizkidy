078a4a2540576976845e4a040e4a1b75a840e92a
FlutLab.io <>
2022-02-25 06:49:58 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,56 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(title: 'Project UTS', debugShowCheckedModeBanner: false, theme: ThemeData(primarySwatch: Colors.green), home: homePage()));
+  runApp(MaterialApp(title: 'Project UTS', debugShowCheckedModeBanner: false, theme: ThemeData(primarySwatch: Colors.green), home: loginPage()));
 }
 
-class homePage extends StatefulWidget {
+class loginPage extends StatefulWidget {
   @override
-  _homePageState createState() => _homePageState();
+  _loginPageState createState() => _loginPageState();
 }
 
-class _homePageState extends State<homePage> {
-  int _selectedNavbar = 0;
-
-  void _changeSelectedNavBar(int index) {
-    setState(() {
-      _selectedNavbar = index;
-    });
-  }
-
+class _loginPageState extends State<loginPage> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: DefaultTabController(
-        length: 4,
-        child: CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              pinned: true,
-              snap: true,
-              floating: true,
-              expandedHeight: 160.0,
-              flexibleSpace: const FlexibleSpaceBar(
-                title: Text('SliverAppBar'),
-                background: Image(
-                  image: AssetImage('asset/bg.jpg'),
-                  fit: BoxFit.cover,
-                ),
+      body: Container(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Positioned(
+              top: 180,
+              left: 120,
+              child: CircleAvatar(
+                radius: 75,
+                backgroundImage: NetworkImage('https://www.google.com/search?q=logo+poliwangi&sxsrf=APq-WBsd6rOAldvCBDglfEJJFouxg45D-Q:1645771604882&tbm=isch&source=iu&ictx=1&vet=1&fir=xIR17ae-4vP1JM%252Cnaq2xMdXjv5jxM%252C_%253BPg78AcsVLusm8M%252CoAhAbwxHYMyN7M%252C_%253BrKsq3rdHuCSIFM%252CoAhAbwxHYMyN7M%252C_%253BX34kdS2fnnDJ5M%252CLGLuiyp3ZZqMBM%252C_%253BtRxSiKEj0U2W-M%252CSP7oBz5f3ilBjM%252C_%253B28dIMJn7j7KEvM%252CSYTE5hR01J5MiM%252C_%253BERlZn5smvCRuhM%252CU2W-5_yLx9_X1M%252C_%253BylmgqBngsqbAtM%252CZv8hZoWmUkKRfM%252C_%253B_Sk9yUZr3claQM%252CnNNjLz796OUo5M%252C_%253Bz6Q8oai2yPSSRM%252C3C3NzVwGz6CDRM%252C_%253B2tLGZlQMlVLc6M%252CoAhAbwxHYMyN7M%252C_%253BPmSSMDVqWc7B8M%252C2bxtWftqMLJ-PM%252C_%253BFabb8c2CWP5aKM%252Cr69oHbYEO8VLcM%252C_%253BNiZ00mf0vHGYcM%252CPsAWVziCbVF8RM%252C_&usg=AI4_-kSnebHXo4Z3ntbJXmCbVXadV5M3ug&sa=X&sqi=2&ved=2ahUKEwizrY__oJr2AhUDG7kGHT-1DLYQ9QF6BAgMEAE#imgrc=X34kdS2fnnDJ5M'),
               ),
             ),
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (BuildContext context, int index) {
-                  return Container(
-                    color: index.isOdd ? Colors.white : Colors.black12,
-                    height: 100.0,
-                    child: Center(
-                      child: Text('$index', textScaleFactor: 5),
-                    ),
-                  );
-                },
-                childCount: 20,
+            Positioned(
+              top: 340,
+              left: 75,
+              child: Text(
+                'SELAMAT DATANG',
+                style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
               ),
             ),
           ],
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Beranda'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.notifications),
-            title: Text('Notifikasi'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.query_builder),
-            title: Text('About'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            title: Text('Akun'),
-          ),
-        ],
-        currentIndex: _selectedNavbar,
-        selectedItemColor: Colors.green,
-        unselectedItemColor: Colors.grey,
-        showUnselectedLabels: true,
-        onTap: _changeSelectedNavBar,
+        // child: Form(
+        //       key: _formKey,
+        //       child: Column(
+        //         children: <Widget>[
+        //           TextFormField(
+        //               validator: (value) {
+        //                 if (value!.isEmpty) {
+        //                   return 'Masukkan nomor peserta anda!';
+        //                 }
+        //               },
+        //           )
+        //         ]
+        //           )
+        //       ),
       ),
     );
   }
