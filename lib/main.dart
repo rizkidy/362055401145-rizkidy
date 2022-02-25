dbf7dec2faa32e78ba1da082c197ddb2dcae276a
FlutLab.io <>
2022-02-25 04:32:06 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(title: 'Project UTS', debugShowCheckedModeBanner: false, home: homePage()));
+  runApp(MaterialApp(title: 'Project UTS', debugShowCheckedModeBanner: false, theme: ThemeData(primarySwatch: Colors.green), home: homePage()));
 }
 
 class homePage extends StatefulWidget {
@@ -21,46 +21,64 @@ class _homePageState extends State<homePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            pinned: true,
-            snap: true,
-            floating: true,
-            expandedHeight: 160.0,
-            flexibleSpace: const FlexibleSpaceBar(
-              title: Text('SliverAppBar'),
-              background: Image(
-                image: AssetImage('asset/bg.jpg'),
+      body: DefaultTabController(
+        length: 4,
+        child: CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              pinned: true,
+              snap: true,
+              floating: true,
+              expandedHeight: 160.0,
+              flexibleSpace: const FlexibleSpaceBar(
+                title: Text('SliverAppBar'),
+                background: Image(
+                  image: AssetImage('asset/bg.jpg'),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return Container(
+                    color: index.isOdd ? Colors.white : Colors.black12,
+                    height: 100.0,
+                    child: Center(
+                      child: Text('$index', textScaleFactor: 5),
+                    ),
+                  );
+                },
+                childCount: 20,
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Beranda'),
           ),
-          BottomNavigationBar(
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                title: Text('Beranda'),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.notifications),
-                title: Text('Notifikasi'),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.question_answer),
-                title: Text('About'),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person),
-                title: Text('Akun'),
-              ),
-            ],
-            currentIndex: _selectedNavbar,
-            selectedItemColor: Colors.green,
-            unselectedItemColor: Colors.grey,
-            showUnselectedLabels: true,
-            onTap: _changeSelectedNavBar,
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            title: Text('Notifikasi'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.query_builder),
+            title: Text('About'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text('Akun'),
           ),
         ],
+        currentIndex: _selectedNavbar,
+        selectedItemColor: Colors.green,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        onTap: _changeSelectedNavBar,
       ),
     );
   }
